25fd48dd09bf47d36df6c68f400ea893106a5521
rachd <rachel.m.dorn@gmail.com>
2017-04-12 16:52:47 -0400

    set up eslint

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import path from 'path';
 import open from 'open';
-import webpack from 'webpack';
-import config from '../webpack.config.dev';
+// import webpack from 'webpack';
+// import config from '../webpack.config.dev';
+
+/* eslint-disable no-console */
 
 const port = 3000;
 const app = express();
-const compiler = webpack(config);
+// const compiler = webpack(config);
 
 // app.use(require('webpack-dev-middeware')(compiler, {
 //     noInfo: true,
